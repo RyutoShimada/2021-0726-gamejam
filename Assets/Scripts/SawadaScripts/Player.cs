a4e028825541f357854d42f969b174dfd8864a7b
SR21C <rysawad@gmail.com>
2021-07-26 14:59:39 +0900

    プレイヤーのスクリプト

--- Assets/Scripts/SawadaScripts/Player.cs
+++ Assets/Scripts/SawadaScripts/Player.cs
@@ -6,36 +6,46 @@ public class Player : MonoBehaviour
 {
     [SerializeField] GameObject pointerPrefab;
     [SerializeField] const float rayTime = 2.0f;
-    private GameObject pointerInstance;
-    private Vector2 pointer;
-    void Start()
-    {
-        
-    }
 
-    
+    GameObject pointerInstance;
+    Vector2 pointer;
+    float PositionX = 0;
+    float PositionY = 0;
+    Vector2 objPos;
+    Vector2 enemyDir;
+    AudioSource audio = null;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     void Update()
     {
-        //カーソル位置
-        float RotationX = Input.mousePosition.x;
-        float RotationY = Input.mousePosition.y;
-
+        PositionX = Input.mousePosition.x;
+        PositionY = Input.mousePosition.y;
         //カーソル位置の取得とワールド座標への変換
-        pointer = new Vector2(RotationX, RotationY);
-        Vector2 objPos = Camera.main.ScreenToWorldPoint(pointer);
+        pointer = new Vector2(PositionX, PositionY);
+        objPos = Camera.main.ScreenToWorldPoint(pointer);
         pointerPrefab.transform.position = objPos;
-    }
 
-    public static RaycastHit2D RaycastAndDraw(Vector2 pointer, Vector2 direction, int layerMask)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(pointer, direction, layerMask);
+        //Rayのコマンド
         if (Input.GetMouseButtonDown(0))
         {
-            //Ray判定
-            //if (hit.collider.gameObject.tag == "terget")
-            //{
-            //    GetComponent<>().Hit();
-            //}
+            enemyDir = pointer - objPos;
+            Raycast();
+        }
+    }
+
+    void Raycast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(objPos, enemyDir);
+        if (hit.collider == null) return;
+
+        //Ray判定
+        if (hit.collider.gameObject.tag == "Target")
+        {
+            audio.Play();
+            //hit.collider.gameObject.GetComponent<Target>().Hit();
         }
     }
 }
