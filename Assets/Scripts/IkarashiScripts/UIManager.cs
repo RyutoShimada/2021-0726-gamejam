26ac94bebb8c77180d7fb41f5b24c5169515b967
tetora ikarashi <teto670013@gmail.com>
2021-07-26 17:19:14 +0900

    no message

--- Assets/Scripts/IkarashiScripts/UIManager.cs
+++ Assets/Scripts/IkarashiScripts/UIManager.cs
@@ -10,7 +10,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] Text m_totalScore;
     private void Start()
     {
-        int total = 100 * GameManager.Instance.m_enemyCount - 50 * GameManager.Instance.m_allyCount;
+        int total = GameManager.Instance.m_score;
         m_enemyText.text = $"{GameManager.Instance.m_enemyCount}";
         m_allyText.text = $"{GameManager.Instance.m_allyCount}";
         m_totalScore.text = $"{total}";
